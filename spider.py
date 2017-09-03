c0ad9daf5cb0e4fc8ee2f5832140130db785075b
Calvin Wu <calvin620707@msn.com>
2017-09-03 18:54:46 +0800

    fix space issue

--- spider.py
+++ spider.py
@@ -25,17 +25,17 @@ class EmailsSpider(scrapy.Spider):
             text_item = comment.css('li.post-text')
             email_domain = text_item.xpath('a/text()').get()
             raw_text = text_item.get()
-            raw_text = raw_text.replace(' ', '')
+            # raw_text = raw_text.replace(' ', '')
             raw_text = raw_text.replace('\n', '')
 
             if email_domain:
-                raw_text = re.sub(r'<atarget=.*</a>', '@' + email_domain, raw_text)
+                raw_text = re.sub(r'<a target=.*</a> ', '@' + email_domain, raw_text)
 
             email_ptn = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
             matched = email_ptn.search(raw_text)
 
             if not matched:
-                yield self._result(floor, None, errors=['email not found'])
+                yield self._result(floor, None, errors=['email not found'], raw_text=raw_text)
                 continue
 
             yield self._result(floor, matched[0])
