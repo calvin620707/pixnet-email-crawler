14d3a85d53cb223db63cdff05abc81271cba8214
Calvin Wu <calvin620707@msn.com>
2017-12-05 22:41:31 +0800

    refine results web

--- crawlers/views.py
+++ crawlers/views.py
@@ -23,4 +23,20 @@ class IndexView(views.View):
         c.get(form.cleaned_data['pixnet_url'])
         with Path(c.scraped_ret_file).open('r') as f:
             ret_json = json.load(f)
+            ret_json = list(map(lambda x: self.update_err_msg(x), ret_json))
             return render(request, 'crawlers/results.html', {'scraped': ret_json})
+
+    def update_err_msg(self, row):
+        if 'errors' not in row:
+            row['err_msg'] = ''
+            return row
+
+        if 'is secret' in row['errors']:
+            row['err_msg'] = "悄悄話"
+        if 'email not found' in row['errors']:
+            if row.get('err_msg'):
+                row['err_msg'] += ', '
+            else:
+                row['err_msg'] = ''
+            row['err_msg'] += "找不到Email"
+        return row
